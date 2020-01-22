8015fb00eac9bec9e247f54478a8d210ebe18a8c
Jatinokov <Enkhtaivan1206@gmail.com>
2020-01-21 22:55:11 -0800

    created mysql database with attemted first 'POST' request

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const multer = require('multer');
+const upload = multer({ dest: 'public/uploads' });
+const mysql = require('mysql')
+
 const PORT = 7000;
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "blog"
+});
+
+db.connect();
+
+
+
+
 
 // handlebars required 
 const exphbs = require('express-handlebars');
@@ -50,6 +67,20 @@ app.get('/create', (req, res)=>{
         title: 'CREATE ARTICLE PAGE',
         style: 'create.css'
     })
+});
+
+app.post('/article', upload.single('image'),  (req, res)=>{
+    res.send("hello");
+    console.log(req.body);
+    console.log(req.file);
+
+    db.query(
+
+        `INSERT INTO articles (headline, subheadline, body) VALUES ('${req.body.headline}', '${req.body.subheadline}', '${req.body.body}')`,
+        
+        function (error, results, fields) {
+        if (error) throw error;
+      });
 })
 
 
