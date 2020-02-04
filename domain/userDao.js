a7fc12a942555b6c56a5d57140ea8127f0de5cb3
Jatinokov <Enkhtaivan1206@gmail.com>
2020-02-04 00:17:13 -0800

    updated mysql data-base domain/userDao.js

--- domain/userDao.js
+++ domain/userDao.js
@@ -1,9 +1,10 @@
 const mysql = require ('mysql');
 const util = require( 'util' );
+require("dotenv").config();
 const conn = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: "blog"
 });
 const query = util.promisify(conn.query).bind(conn);
